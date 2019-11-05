e415bf048d62b951aa580fe49c64bdfdda83167b
Alexander Bispo <alexbispo173@gmail.com>
2019-11-05 02:38:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessible :a,:b,:c
+  
+  
+  def initialize(a,b,c)
+    @a=a
+    @b=b 
+    @c=c
+  end
+  
+  def kind 
+    
+    if (a == b == c)
+      :equilateral
+    elsif (a == b != c)||(a != b == c)||(a == b != c)
+      :isosceles
+    elsif (a != b! == c)
+      :scalene
+    
+  
+  end
+  
+  
+    
 end
