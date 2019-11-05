cd0dabbede8551724299bcaed54eeab5f6ec0f6d
Alexander Bispo <alexbispo173@gmail.com>
2019-11-05 02:42:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,22 +1,49 @@
-class Triangle
-  attr_accessible :a,:b,:c
-  
-  
-  def initialize(a,b,c)
-    @a=a
-    @b=b 
-    @c=c
+attr_accessor :side1, :side2, :side3
+  @sides = []
+
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
   end
-  
-  def kind 
-    if (a = b = c)
+
+  def kind
+    if(negative? == true || valid? == false)
+      raise TriangleError
+    elsif(equilateral?)
       :equilateral
-    elsif (a = b != c)||(a != b = c)||(a = b != c)
-      :isosceles
-    elsif (a != b! == c)
+    elsif(scalene?)
       :scalene
+    elsif(isosceles?)
+      :isosceles
+    end
   end
-  
-  
-    
+
+  def negative?
+    @sides.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def equilateral?
+    (@side1 == @side2) && (@side1 == @side3)
+  end
+
+  def scalene?
+    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
+  end
+
+  def isosceles?
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
+  end
+
+  def valid?
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
